8e4936480f64b3d651ee6f4a1e52c6b67a025d21
PengChuan <summer_in_2014@163.com>
2016-10-25 23:08:37 +0800

    bugfix，ajax请求url
    getWebRootPath() 方法可能返回"/"，在 ajax 调用url: getWebRootPath() + "/getFollowedUsers"，使得ajax请求url为"http://getFollowedUsers"。目前不考虑web根目录，getWebRootPath()直接返回""。

--- webapp/src/main/webapp/js/index.js
+++ webapp/src/main/webapp/js/index.js
@@ -82,10 +82,10 @@ function findDetail(name) {
 }
 
 function getWebRootPath() {
-    var webroot = document.location.href;
-    webroot = webroot.substring(webroot.indexOf('//') + 2, webroot.length);
-    webroot = webroot.substring(webroot.indexOf('/') + 1, webroot.length);
-    webroot = webroot.substring(0, webroot.indexOf('/'));
-    var rootpath = "/" + webroot;
-    return rootpath;
+    //var webroot = document.location.href;
+    //webroot = webroot.substring(webroot.indexOf('//') + 2, webroot.length);
+    //webroot = webroot.substring(webroot.indexOf('/') + 1, webroot.length);
+    //webroot = webroot.substring(0, webroot.indexOf('/'));
+    //var rootpath = "/" + webroot;
+    return "";
 }
