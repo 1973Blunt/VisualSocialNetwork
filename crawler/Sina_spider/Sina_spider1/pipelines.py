fd97f6700c5c781e1e3b8a3044f4c037635a4d38
Chuan <summer15y@163.com>
2016-12-31 16:19:13 +0800

    bugfix，wb_usr_id统一为整型
    wb_usr_id在neo4j的数据类型可能为整型或字符串，修正方法是，在pipelines.py在存入数据库前，将wb_usr_id全部转换为int型。

--- crawler/Sina_spider/Sina_spider1/pipelines.py
+++ crawler/Sina_spider/Sina_spider1/pipelines.py
@@ -13,28 +13,30 @@ class Neo4jDBPipleline(object):
     def process_item(self, item, spider):
         """ 判断item的类型，并作相应的处理，再入数据库 """
         if isinstance(item, InformationItem):
+            item["wb_usr_id"] = int(item["wb_usr_id"])
             usr = Node("WeiboUser", **dict(item))
             self.db.merge(usr, "WeiboUser", "wb_usr_id")
         elif isinstance(item, TweetsItem):
+            item["wb_usr_id"] = int(item["wb_usr_id"])
             weibo = Node("WeiboTweets", **dict(item))
             self.db.merge(weibo, "WeiboTweets", "wb_tt_id")
-            usr = Node("WeiboUser", wb_usr_id=weibo["wb_usr_id"])
+            usr = Node("WeiboUser", wb_usr_id=int(weibo["wb_usr_id"]))
             fan_follow_sb = Relationship(usr, "TWEETS", weibo)
             self.db.merge(fan_follow_sb)
         elif isinstance(item, FollowsItem):
             followsItems = dict(item)
             follows = followsItems.pop("follows")
-            fan = Node("WeiboUser", wb_usr_id=followsItems["wb_usr_id"])
+            fan = Node("WeiboUser", wb_usr_id=int(followsItems["wb_usr_id"]))
             for sb_id in follows:
-                sb = Node("WeiboUser", wb_usr_id=sb_id)
+                sb = Node("WeiboUser", wb_usr_id=int(sb_id))
                 fan_follow_sb = Relationship(fan, "FOLLOWS", sb)
                 self.db.merge(fan_follow_sb)
         elif isinstance(item, FansItem):
             fansItems = dict(item)
             fans = fansItems.pop("fans")
-            sb = Node("WeiboUser", wb_usr_id=fansItems["wb_usr_id"])
+            sb = Node("WeiboUser", wb_usr_id=int(fansItems["wb_usr_id"]))
             for fan_id in fans:
-                fan = Node("WeiboUser", wb_usr_id=fan_id)
+                fan = Node("WeiboUser", wb_usr_id=int(fan_id))
                 fan_follow_sb = Relationship(fan, "FOLLOWS", sb)
                 self.db.merge(fan_follow_sb)
         return item
